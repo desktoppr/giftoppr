eee3d1cd90e5d3823b5d6e3fe58bee6a236a8c4f
Keith Pitt <me@keithpitt.com>
2013-02-04 09:00:11 +1100

    Blow up if the secret and key are not set.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,11 @@
 OmniAuth.config.logger = Rails.logger
 
+dropbox_key = ENV['DROPBOX_KEY']
+raise "Missing env variable DROPBOX_KEY" unless dropbox_key.present?
+
+dropbox_secret = ENV['DROPBOX_SECRET']
+raise "Missing env variable DROPBOX_SECRET" unless dropbox_secret.present?
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :dropbox, ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET']
+  provider :dropbox, dropbox_key, dropbox_secret
 end
