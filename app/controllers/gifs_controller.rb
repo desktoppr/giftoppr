391341f161e6ffb9c848c4177701ae1790e13e87
Keith Pitt <me@keithpitt.com>
2013-02-06 19:53:40 +1100

    Updated gifs controller.

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,9 +1,17 @@
 class GifsController < ApplicationController
+  respond_to :json
+
+  def index
+    @gifs = Gif.latest.page(params[:page]).per(20)
+
+    respond_with @gifs
+  end
+
   def download
     if user_signed_in?
       Dropbox::Copier.copy_to_users_dropbox current_user.id, params[:id]
     end
 
-    render :nothing => true
+    respond_with :ok => true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Giftoppr::Application.routes.draw do
-  post '/gifs/:id/download', :to => 'gifs#download', :as => 'gif_download'
-
   scope "/auth" do
     match 'logout', :to => 'sessions#destroy', :as => 'auth_logout'
     match 'failure', :to => 'sessions#failure'
@@ -8,6 +6,12 @@ Giftoppr::Application.routes.draw do
     # /auth/:provider is caught by the middleware. Here is just a named route
     match ':provider', :to => lambda { |env| [404, {}, ["Not Found"]] }, :as => 'auth'
     match ':provider/callback', :to => 'sessions#create'
+  end
+
+  resources :gifs, :only => [ :index ] do
+    member do
+      post :download
+    end
   end
 
   root :to => 'public#index'
