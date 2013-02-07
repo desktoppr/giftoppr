4df78e6b832b2548dc26060f1a177af4f973118b
Keith Pitt <me@keithpitt.com>
2013-02-07 17:48:04 +1100

    Revert "Updated gifs controller."
    This reverts commit 391341f161e6ffb9c848c4177701ae1790e13e87.

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,12 +1,4 @@
 class GifsController < ApplicationController
-  respond_to :json
-
-  def index
-    @gifs = Gif.latest.page(params[:page]).per(20)
-
-    respond_with @gifs
-  end
-
   def download
     if user_signed_in?
       Dropbox::Copier.copy_to_users_dropbox current_user.id, params[:id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Giftoppr::Application.routes.draw do
+  post '/gifs/:id/download', :to => 'gifs#download', :as => 'gif_download'
+
   scope "/auth" do
     match 'logout', :to => 'sessions#destroy', :as => 'auth_logout'
     match 'failure', :to => 'sessions#failure'
@@ -6,12 +8,6 @@ Giftoppr::Application.routes.draw do
     # /auth/:provider is caught by the middleware. Here is just a named route
     match ':provider', :to => lambda { |env| [404, {}, ["Not Found"]] }, :as => 'auth'
     match ':provider/callback', :to => 'sessions#create'
-  end
-
-  resources :gifs, :only => [ :index ] do
-    member do
-      post :download
-    end
   end
 
   root :to => 'public#index'
