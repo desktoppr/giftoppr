941ad978572039cc6e8729023496878e8211e098
Keith Pitt <me@keithpitt.com>
2013-02-17 12:56:38 +1100

    Seed development environment using gifs from your Dropbox folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+DROPBOX_PATH = File.expand_path "~/Dropbox/Apps/Giftoppr"
+
+users = if User.count > 0
+          User.all.to_a
+        else
+          %w(keithpitt mariovisic).map do |username|
+            User.create! :provider => 'dropbox', :uid => Time.now.to_i, :name => username, :email => "#{username}@example.com", :oauth_token => 'hello', :oauth_secret => 'hello'
+          end
+        end
+
+gifs = Dir[File.join DROPBOX_PATH, "*"]
+
+gifs.each do |path|
+  size = FastImage.size(path)
+
+  if size
+    unique_hash = `md5 -r "#{path}"`.split(' ')[0]
+
+    gif =  Gif.new :file => File.new(path), :width => size[0], :height => size[1], :unique_hash => unique_hash, :bytes => File.size(path)
+    gif.uploader = users.sample
+    gif.save!
+
+    p gif
+  else
+    puts "Could not calculate size for #{path}"
+  end
+end
