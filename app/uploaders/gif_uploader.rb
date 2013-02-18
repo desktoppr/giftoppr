f315635f992a2b6e14c4b8e8798c2ebf77195775
Mario Visic <mario@mariovisic.com>
2013-02-18 19:26:54 +1100

    Do not optimize uploaded images just yet

--- app/uploaders/gif_uploader.rb
+++ app/uploaders/gif_uploader.rb
@@ -8,8 +8,6 @@ class GifUploader < CarrierWave::Uploader::Base
   end
 
   version :preview do
-    process :quality_and_strip => 80
-
     def full_filename(*args)
       super.chomp(File.extname(super)) + '.jpg' # Force extension to be jpg
     end
@@ -23,19 +21,5 @@ class GifUploader < CarrierWave::Uploader::Base
   # filenames
   def filename
     original_filename.try(:gsub, '+', '-')
-  end
-
-  private
-
-  # Run all the commands in 1 manipulate block to limit the amount of IO
-  # stuff that has to happen on a single upload
-  def quality_and_strip(percentage)
-    manipulate! do |img|
-      img.format('jpg') # We want to enforce jpeg so we can use good compression.
-      img.strip # Do not store EXIF data in the thumb to save space
-      img.quality(percentage.to_s)
-      img = yield(img) if block_given?
-      img
-    end
   end
 end
