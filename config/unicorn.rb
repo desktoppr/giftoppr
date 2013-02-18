cf0d235749bfa6ba9c1fa34c5786306d861f5a18
Mario Visic <mario@mariovisic.com>
2013-02-18 19:39:50 +1100

    Fix issue with unicorn and activerecord on heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,3 +12,14 @@ else
   port = ENV['PORT'].to_i
   listen port, :tcp_nopush => false
 end
+
+# Master process disconnects from active record so it is not available
+# to children.
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+end
+
+# Children each create a new active record connection.
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
