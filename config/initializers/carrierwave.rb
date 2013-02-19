3a1ea7384a6c4c4d83bc4f7193b90ee70fd84269
Keith Pitt <me@keithpitt.com>
2013-02-19 20:43:29 +1100

    More updates to env variables.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,15 +13,15 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY_ID'],
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
       :persistent             => false # This is required to prevent write timeouts from PUT requests to S3
     }
 
-    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_directory  = ENV['FOG_DIRECTORY']
     config.fog_public     = true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-    config.asset_host     = ENV['S3_HOST']
+    config.asset_host     = ENV['FOG_HOST']
   else
     config.storage = :file
   end
