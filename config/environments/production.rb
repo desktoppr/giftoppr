bb77d5b532a26888a2e7fc98c1c2ae574988dd63
Mario Visic <mario@mariovisic.com>
2013-02-20 22:51:23 +1100

    Embed the google font loader to avoid extra JS call

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Giftoppr::Application.configure do
   config.action_controller.asset_host = ENV['FOG_HOST']
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( webfont.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
