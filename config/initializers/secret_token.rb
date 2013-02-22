6ddf6e159a2ed7045d0ad143140825ab324ff135
Mario Visic <mario@mariovisic.com>
2013-02-22 12:27:05 +1100

    Store secret token in env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Giftoppr::Application.config.secret_token = 'b4e7acc959cae51cfe307940fb77354c0ad318bc68008ba039f3fd04ceddd8fe3e76e56a1f1c0f038a9889a328ee31a5716ee5f0d0a2712cf7452f07bb4a9455'
+Giftoppr::Application.config.secret_token = ENV['SECRET_TOKEN']
