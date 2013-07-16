fa350288da0575a1d161be54738452cb24028636
Jeremy Tennant <me@j10.io>
2013-07-16 23:17:58 +1000

    add basic model spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "should respond to its attributes" do
+    it { should respond_to(:provider) }
+    it { should respond_to(:uid) }
+    it { should respond_to(:name) }
+    it { should respond_to(:email) }
+    it { should respond_to(:oauth_token) }
+    it { should respond_to(:oauth_token) }
+    it { should respond_to(:created_at) }
+    it { should respond_to(:updated_at) }
+    it { should respond_to(:change_cursor) }
+  end
+
+  describe "should only allow valid attributes" do
+    it { should validate_presence_of(:provider) }
+    it { should validate_presence_of(:uid) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:oauth_token) }
+    it { should validate_presence_of(:oauth_secret) }
+  end
+
 end
