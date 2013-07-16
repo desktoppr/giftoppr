a8933d81c86bd14812c15037d4dfaaa544b9f3fa
Jeremy Tennant <me@j10.io>
2013-07-16 23:33:03 +1000

    add resources route (albeit limited) for users

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Giftoppr::Application.routes.draw do
 
   get '/:image.gif' => 'images#show'
 
+  resources :users, only: [:show, :destroy]
+
   root :to => 'public#index'
   get 'how-does-giftoppr-work', to: 'public#how'
 end
