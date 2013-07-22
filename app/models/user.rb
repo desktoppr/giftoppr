ceaef887ba3ba92c762b0aff633c5503fa216382
Jeremy Tennant <me@j10.io>
2013-07-22 23:08:54 +1000

    fix bug, class name and foreign key required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :images
+  has_many :images, class_name: "Image",
+                          foreign_key: "uploader_id"
 
   validates :provider, :uid, :name, :email, :oauth_token, :oauth_secret, :presence => true
 end
