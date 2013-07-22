668e0a506973b136cc33d67deb7bf5c7a7f62fca
Jeremy Tennant <me@j10.io>
2013-07-22 23:10:24 +1000

    Remove filter/restriction from the users#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :redirect_unless_authorised
 
   def show
     @user = current_resource
-    @images = current_resource.images.latest.page(params[:page]).per(30)
+    @images = current_resource.images.page(params[:page]).per(30)
   end
 
   private
-
-    def redirect_unless_authorised
-      redirect_to root_path unless user_signed_in? && current_user?(current_resource)
-    end
 
     def current_resource
       @current_resource ||= User.find(params[:id]) if params[:id]
